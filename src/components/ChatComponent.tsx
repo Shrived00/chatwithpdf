7b75a14229af6d8a53c0c379ea5839ef431e4f0e
Shrived00 <shrivedkakde@gmail.com>
2024-08-16 18:08:23 +0530

    context done

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -6,13 +6,30 @@ import { Send } from 'lucide-react';
 import { Button } from './ui/button';
 import MessageList from './MessageList';
 
-const ChatComponent = () => {
+type Props = { chatId: number };
+
+
+const ChatComponent = ({ chatId }: Props) => {
 
     const { input, handleInputChange, handleSubmit, messages } = useChat({
         api: "/api/chat",
+        body: {
+            chatId
+        }
     });
+
+    React.useEffect(() => {
+        const messageContainer = document.getElementById("message-container");
+        if (messageContainer) {
+            messageContainer.scrollTo({
+                top: messageContainer.scrollHeight,
+                behavior: "smooth",
+            });
+        }
+    }, [messages]);
+
     return (
-        <div className='relative min-h-screen '>
+        <div className='relative min-h-screen ' id='message-container'>
             <div className="sticky top-0 inset-x-0 bg-white p-2 h-fit">
                 <h3 className='text-xl font-bold'>Chat</h3>
             </div>
@@ -22,7 +39,7 @@ const ChatComponent = () => {
 
             <MessageList messages={messages} />
 
-            <form onSubmit={handleSubmit} className='sticky bottom-0 inset-x-0 p-2 py-4  bg-white'>
+            <form onSubmit={handleSubmit} className='sticky flex bottom-0 inset-x-0 p-2 py-4  bg-white'>
                 <Input value={input} onChange={handleInputChange} placeholder='Ask any Question ?' className='w-full' />
                 <Button className='bg-blue-600 ml-2'>
                     <Send className='h-4 w-4' />
